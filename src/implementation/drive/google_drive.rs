bb61c10298dbd8284469bf428d64f992ef7b197a
anud12 <supstanta_andu@yahoo.com>
2023-07-09 11:10:49 +0000

    commit

--- src/implementation/drive/google_drive.rs
+++ src/implementation/drive/google_drive.rs
@@ -22,12 +22,12 @@ impl FileQuery<GoogleDriveFile> for GoogleDrive {
         &self,
         query_request: crate::api::file::RequestList,
     ) -> Result<Vec<GoogleDriveFile>, Box<dyn Error>> {
-        info!("query_list {:?}", query_request);
+        println!("query_list {:?}", query_request);
         google_drive_query(&self.session, query_request)
     }
 
     fn query_one(&self, query_request: RequestOne) -> Result<GoogleDriveFile, Box<dyn Error>> {
-        info!("query_one {:?}", query_request);
+        println!("query_one {:?}", query_request);
         google_drive_query_one(&self.session, query_request)
     }
 
