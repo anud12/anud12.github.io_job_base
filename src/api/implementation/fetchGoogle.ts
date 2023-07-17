b75348a224ccfccb15bc2969565fa05cff300991
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-07-17 15:10:08 +0300

    commit

--- src/api/implementation/fetchGoogle.ts
+++ src/api/implementation/fetchGoogle.ts
@@ -2,10 +2,11 @@ import fetch, {Response} from "node-fetch";
 
 export const fetchGoogle = (...args:Parameters<typeof fetch>):Promise<Response> => {
   return new Promise(async (resolve, reject) => {
-    const response = await fetch(args[0], {
+    const responsePromise = fetch(args[0], {
       ...args[1],
     });
     setTimeout(async () => {
+      const response = await responsePromise
       if(response.status !== 200) {
         reject(Error(`${response.status} ${response.statusText}: ${await response.text()}`));
         return;
