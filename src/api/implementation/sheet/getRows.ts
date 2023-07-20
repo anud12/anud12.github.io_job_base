b5db6b994236f56285929ebd70eb8c6cd382ca93
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-07-20 09:11:47 +0300

    commit

--- src/api/implementation/sheet/getRows.ts
+++ src/api/implementation/sheet/getRows.ts
@@ -7,9 +7,7 @@ import {fetchGoogle} from "../fetchGoogle";
 export const getRows = async (sheet: GoogleSheet, query: TableQuery): Promise<[number, string[]][]> => {
   const query_skip = query.skip || 1;
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheet.fileData.id}/values`;
-  const urlWithSize = query.size
-    ? `${url}/Sheet1!${query_skip + 1}:${query.size}`
-    : url;
+  const urlWithSize = `${url}/Sheet1!${query_skip + 1}:${query.size ?? 40000}`;
 
   const urlWithDimension = `${urlWithSize}?majorDimension=ROWS`;
   const response = await fetchGoogle(urlWithDimension, {
